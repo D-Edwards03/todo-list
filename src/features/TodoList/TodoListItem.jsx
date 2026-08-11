a154b7277f260bb29aa19307fb5553716ea5bae8
D-Edwards03 <meachie314@gmail.com>
2026-08-11 11:09:03 -0500

    Custom hook

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,34 +1,31 @@
-import { useState } from "react";
 import TextInputWithLabel from "../../shared/TextInputWithLabel";
 import { isValidTodoTitle } from "../../utils/todoValidation";
+import { useEditableTitle } from "../../hooks/useEditableTitle";
 
 function TodoListItem({ todo, onCompleteTodo, onUpdateTodo }) {
-  const [isEditing, setIsEditing] = useState(false);
-  const [workingTitle, setWorkingTitle] = useState(todo.title);
-
-  const handleCancel = () => {
-    setWorkingTitle(todo.title);
-    setIsEditing(false);
-  };
-
-  const handleEdit = (event) => {
-    setWorkingTitle(event.target.value);
-  };
+  const {
+    isEditing,
+    workingTitle,
+    startEditing,
+    cancelEdit,
+    updateTitle,
+    finishEdit
+  } = useEditableTitle(todo.title)
 
   const handleUpdate = (event) => {
     if (!isEditing) return;
 
     event.preventDefault();
-    onUpdateTodo({ ...todo, title: workingTitle });
-    setIsEditing(false);
+    const finalTitle = finishEdit();
+    onUpdateTodo({ ...todo, title: finalTitle });
   };
 
   return (
     <li>
       {isEditing ? (
         <form onSubmit={handleUpdate}>
-          <TextInputWithLabel value={workingTitle} onChange={handleEdit} />
-          <button type="button" onClick={handleCancel}>
+          <TextInputWithLabel value={workingTitle} onChange={(event) => updateTitle(event.target.value)} />
+          <button type="button" onClick={cancelEdit}>
             Cancel
           </button>
 
@@ -47,7 +44,7 @@ function TodoListItem({ todo, onCompleteTodo, onUpdateTodo }) {
             checked={todo.isCompleted}
             onChange={() => onCompleteTodo(todo.id)}
           />
-          <span onClick={() => setIsEditing(true)}>{todo.title}</span>
+          <span onClick={startEditing}>{todo.title}</span>
         </>
       )}
     </li>
