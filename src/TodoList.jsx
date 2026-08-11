f3c3a1470903718ece8770b143a6aea7d71f2af1
D-Edwards03 <meachie314@gmail.com>
2026-08-10 22:15:59 -0500

    Add empty state message

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,9 +2,13 @@ import TodoListItem from "./TodoListItem";
 
 function TodoList({todoList}) {
   return (
+    todoList.length === 0 ? (
+      <p>Add todo above to get started</p>
+    ) : (
     <ul>
         {todoList.map(todo => (<TodoListItem key={todo.id} todo={todo} />))}
     </ul>
+    )
   );
 }
 
